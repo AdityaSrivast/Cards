0c2e04baf6d2a907026bdd1e4bebf98b768f43af
AdityaSrivast <yashsri421@gmail.com>
2018-04-18 10:10:49 +0530

    Modify Animation

--- js/script.js
+++ js/script.js
@@ -3,13 +3,21 @@ $(document).ready(function(){
       var2="<p class='card'></p>";
 
         $("#btn1").click(function(){
+          //$(".card-wrapper").append(var2);
           $(".card-wrapper").append(var2);
+          
         
         });
 
       $("#btn2").click(function(){
+      	$("p").last().slideUp(300,function(){
+      		$("p").last().remove();
+
+      	});
         
-        $("p").last().remove();
+        //$("p").last().remove();
+        
+       // $("p").show(1000);
 
       });
     });
